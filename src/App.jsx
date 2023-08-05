64b6a32a99abecf7c4bc0dda7ed992802407b986
Mario-Chavez <marioachavez1@gmail.com>
2023-08-05 01:03:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <>
+            <h1>soy la web</h1>
+        </>
+    );
+}
+
+export default App;
