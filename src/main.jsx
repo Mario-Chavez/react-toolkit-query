acb533008c736c89015c3c729b91151e1d801807
Mario-Chavez <marioachavez1@gmail.com>
2023-08-08 11:10:56 -0300

    se comento codigo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice.js";
-
+// se provee una api
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ApiProvider api={apiSlice}>
